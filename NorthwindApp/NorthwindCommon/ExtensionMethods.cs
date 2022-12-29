cabd9d2d49bf872927f26d4982c33b7f861a7b88
Ashib Hoque <a.hoque29@hotmail.com>
2022-12-29 01:51:06 +0000

    Added ExtensionMethods.cs delete function.

---
+++ NorthwindApp/NorthwindCommon/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace NorthwindCommon;
+
+public static class ExtensionMethods
+{
+	public static void Delete<TEntity>(this DbSet<TEntity> entities, Expression<Func<TEntity, bool>> predicate)
+		where TEntity : class
+	{
+		var records = entities.Where(predicate).ToList();
+
+		if (records.Any()) entities.RemoveRange(records);
+	}
+}
