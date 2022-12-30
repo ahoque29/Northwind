f5e3b03449b0dfb47f39a2e5efac163e16e8cf68
Ashib Hoque <a.hoque29@hotmail.com>
2022-12-30 17:15:11 +0000

    Registered entitywrite services into program

--- NorthwindApp/NorthwindClient/Program.cs
+++ NorthwindApp/NorthwindClient/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NorthwindClient;
+using NorthwindClientService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,6 +9,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("Client", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Client"));
+
+builder.Services.AddScoped<IEntityCreateService, EntityCreateService>();
+builder.Services.AddScoped<IEntityEditService, EntityEditService>();
+builder.Services.AddScoped<IEntityUpsertService, EntityUpsertService>();
+builder.Services.AddScoped<IEntityRemoveService, EntityRemoveService>();
+builder.Services.AddScoped<IEntityRemoveAllService, EntityRemoveAllService>();
+
 
 builder.Services.AddOptions();
 
