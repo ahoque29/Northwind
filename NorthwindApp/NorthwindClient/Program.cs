33a828a9aa4f6ace6eb58d65c6a6038f9647574a
Ashib Hoque <a.hoque29@hotmail.com>
2023-01-01 14:42:00 +0000

    starting northwindclient from scratch

--- NorthwindApp/NorthwindClient/Program.cs
+++ NorthwindApp/NorthwindClient/Program.cs
@@ -1,30 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NorthwindClient;
-using NorthwindClientService;
-using Radzen;
-using Radzen.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("Client", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Client"));
-
-builder.Services.AddScoped<DialogService>();
-builder.Services.AddScoped<NotificationService>();
-builder.Services.AddScoped<RadzenContextMenu>();
-builder.Services.AddScoped<RadzenTooltip>();
-
-builder.Services.AddScoped<IReadOnlyService, ReadOnlyService>();
-
-builder.Services.AddScoped<IEntityCreateService, EntityCreateService>();
-builder.Services.AddScoped<IEntityEditService, EntityEditService>();
-builder.Services.AddScoped<IEntityUpsertService, EntityUpsertService>();
-builder.Services.AddScoped<IEntityRemoveService, EntityRemoveService>();
-builder.Services.AddScoped<IEntityRemoveAllService, EntityRemoveAllService>();
-
-builder.Services.AddOptions();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
